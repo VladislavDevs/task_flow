e8d079000b3fded7dd2d577cb95615e243bb69ff
dartmew <dartmew@yandex.com>
2026-05-12 18:41:37 +0300

    Внес в схему task.py TaskOut/TaskListOut

--- backend/app/schemas/task.py
+++ backend/app/schemas/task.py
@@ -1,6 +1,7 @@
 # backend/app/schemas/task.py
 from pydantic import BaseModel
 from typing import Optional
+from app.models.task import TaskStatus
 
 class TaskCreate(BaseModel):
     name: str
@@ -8,8 +9,28 @@ class TaskCreate(BaseModel):
     category_id: Optional[int] = None
     initial_assessment_seconds: Optional[int] = None
 
+
 class TaskUpdate(BaseModel):
     name: Optional[str] = None
     description: Optional[str] = None
     category_id: Optional[int] = None
     initial_assessment_seconds: Optional[int] = None
+
+
+class TaskOut(BaseModel):
+    id: int
+    name: str
+    description: Optional[str] = None
+    status: TaskStatus
+    initial_assessment_seconds: Optional[int] = None
+    final_assessment_seconds: Optional[int] = None
+    category_id: Optional[int] = None
+    user_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class TaskListOut(BaseModel):
+    total: int
+    items: list[TaskOut]
